844e57658941b13179c687f4f563669318affa50
Dineshkumar <dineshkumarbemba27@gmail.com>
2026-07-29 08:47:27 +0530

    Assignment 1-Datatypes

---
+++ javascript-training/hello-world.js
@@ -0,0 +1,21 @@
+console.log("Hello, Aadhira....!")
+
+console.info("this is information")
+console.warn("this is warning message")
+console.error("this is error message")
+let a="Ramya"
+{
+    let a="dinesh";
+    const b="Aadhira"
+    console.log(a);
+    console.log(b);
+}
+console.log(a)
+
+
+{
+    let a ="Ramya";
+    const b="Aadhira"
+    console.log(a);
+    console.log(b);
+}
---
+++ javascript-training/non-primitive.js
@@ -0,0 +1,12 @@
+let empName=Symbol();
+let empData={
+    [empName]:"Dinesh",
+    "empId":52107621,
+    "visaStatus":true,
+    "address":{
+        "street":"9/109,Ramnagar,Poosaripatti",
+        "city":"Salem",
+        "State":"Tamilnadu"
+    }
+}
+console.log(empData)
